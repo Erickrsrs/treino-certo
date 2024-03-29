2f0d23c9857b87a3547fbd09828ae54c156e7895
Erick Rocha <erick.r.souza@cogna.com.br>
2024-03-29 15:47:09 -0300

    chore: add methods to find user by id and email

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -5,6 +5,14 @@ import { UsersRepository } from '../users-repository'
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = []
 
+  async findById(id: string): Promise<User | null> {
+    return this.items.find((user) => user.id === id) || null
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.items.find((user) => user.email === email) || null
+  }
+
   async create(data: Prisma.UserCreateInput): Promise<User> {
     const user = {
       id: String(randomUUID()),
