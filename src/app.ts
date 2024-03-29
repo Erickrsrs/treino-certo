035d606ae9ed92a3f9a8c0daf3545f9602d3a416
Erick Rocha <erick.r.souza@cogna.com.br>
2024-03-29 14:49:12 -0300

    chore: adds global error validation

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,17 @@
 import fastify from 'fastify'
+import { ZodError } from 'zod'
 
 export const app = fastify()
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    reply.status(400).send({
+      message: 'Validation error',
+      issues: error.errors.map((err) => err.message),
+    })
+  }
+
+  return reply.status(500).send({
+    message: 'Internal server error',
+  })
+})
